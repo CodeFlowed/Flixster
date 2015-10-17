3744149e8b4c68dbc4f2aa112859d48bf96869b8
CodeFlowed <syncstep@gmail.com>
2015-10-17 03:32:52 +0000

    Create form for adding new lesson

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,2 +1,10 @@
 class Instructor::LessonsController < ApplicationController
+  def new
+    @section = Section.find(params[:section_id])
+    @lesson = Lesson.new
+  end
+
+  # def create
+  #   @lesson =
+  # end
 end
